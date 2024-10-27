c95e6d4c221ce5f8ac9d5d81930f39cebab29802
Alina <alia.nord.adventurer@gmail.com>
2024-10-27 11:48:08 +0100

    hmmm

--- src/utils/emailClient.js
+++ src/utils/emailClient.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 
-import { SMTP } from '../constants/index.js';
+import { SMTP } from '../constants/constants.js';
 import { env } from './env.js';
 
 export const emailClient = nodemailer.createTransport({
